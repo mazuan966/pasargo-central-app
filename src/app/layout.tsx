464b7dca1f7c40c101cee5a34c8d0d3852854344
Mazuan Masri <MazuanEwan@gmail.com>
2025-07-02 09:43:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin=""/>
       </head>
       <body className="font-body antialiased">
-        <CartProvider>
-          <OrderProvider>
+        <OrderProvider>
+          <CartProvider>
             {children}
-          </OrderProvider>
-        </CartProvider>
+          </CartProvider>
+        </OrderProvider>
         <Toaster />
       </body>
     </html>
