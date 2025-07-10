a9017b5a7c5123d643b9dce62abea4fccfdceab8
Mazuan Masri <MazuanEwan@gmail.com>
2025-07-10 00:32:32 +0000

    the problem is, now using this studio i can access . but when i click pu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,22 +12,17 @@ const firebaseConfig = {
   appId: "1:909679104927:web:632c43341dc54c1ac6a3aa"
 };
 
-let app: FirebaseApp | undefined;
-let auth: Auth | undefined;
-let db: Firestore | undefined;
+let app: FirebaseApp;
+let auth: Auth;
+let db: Firestore;
 
-try {
-  if (getApps().length === 0) {
-    app = initializeApp(firebaseConfig);
-  } else {
-    app = getApps()[0];
-  }
-  auth = getAuth(app);
-  db = getFirestore(app);
-} catch (error) {
-  console.error("Firebase initialization error:", error);
-  // In a production environment, you might want to handle this more gracefully.
-  // For now, we'll allow the app to run with services being undefined.
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
 }
+
+auth = getAuth(app);
+db = getFirestore(app);
 
 export { app, auth, db };
